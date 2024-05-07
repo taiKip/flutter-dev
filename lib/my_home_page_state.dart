21321f2ec0135d8c44705ca6accff88d6199d0d5
tarus <victortarus6@gmail.com>
2024-05-07 13:00:42 +0300

    Changed fontSize

--- lib/my_home_page_state.dart
+++ lib/my_home_page_state.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class _MyHomePageState extends StatelessWidget{
+class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      color: Colors.orange,
+      // width: 200,
+      // height: 100,
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
+      child: const Text(
+        'Hello, ninjas',
+        style: TextStyle(
+            fontSize: 18,
+            letterSpacing: 4,
+            decoration: TextDecoration.underline,
+            fontStyle: FontStyle.italic),
+      ),
+    );
   }
-  
 }
